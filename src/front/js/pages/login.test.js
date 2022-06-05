db3774bbe088260b66d09a14d9132e2be0adee95
Dhaval rk dk <pdhaval988@gmail.com>
2022-06-05 14:59:50 +0000

    login

--- src/front/js/pages/login.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import loginpage from "./Loginpage";
-
-test("renders learn react link", () => {
-  render(<loginpage />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
